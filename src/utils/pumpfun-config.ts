cb584f2503549b3611bf1cddf3eab3ec9f16e95c
Carlos Peralta <carlos@blackmouthgames-com>
2025-06-06 18:53:05 +0200

    CA

--- src/utils/pumpfun-config.ts
+++ src/utils/pumpfun-config.ts
@@ -1,3 +1,3 @@
 // Configuración de la URL de compra de $SDFT en Pump Fun
-export const PUMPFUN_URL = "https://pump.fun/";
-export const CA_TEXT = "coming soon"; 
+export const PUMPFUN_URL = "https://pump.fun/coin/Eb63j34KkiyNB1bCSXEURP5Zg5jUXmEdkbVmXBVzpump";
+export const CA_TEXT = "Eb63j34KkiyNB1bCSXEURP5Zg5jUXmEdkbVmXBVzpump"; 
