d01fb7b8ce2c80a23227586d2386fa35e5899d5b
Carlos Peralta <carlos@blackmouthgames-com>
2025-06-06 19:13:33 +0200

    dexscreener updated

--- src/utils/links-config.ts
+++ src/utils/links-config.ts
@@ -1,7 +1,7 @@
 // Configuración centralizada de enlaces
 export const LINKS = {
   PUMPFUN: "https://pump.fun/coin/Eb63j34KkiyNB1bCSXEURP5Zg5jUXmEdkbVmXBVzpump",
-  DEXSCREENER: "https://dexscreener.com",
+  DEXSCREENER: "https://dexscreener.com/solana/9svqrkfwl8ykb9n47vovv8fxiwk9tjeirxeory8trfrz",
   X: "https://x.com/FDFT_tarot",
   TELEGRAM: "https://t.me/fudfate",
   BUY_NOW_TICKER: "https://pump.fun/"
